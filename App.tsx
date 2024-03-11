4d4710bec45a4bc9617855f551858b8fce720a31
Mattijs Fuijkschot <mattijs@gaiyo.com>
2024-03-11 09:35:39 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import MapboxScrollViewBug from "./MapboxScrollViewBug"
+
+export default function App(): React.JSX.Element {
+  return <MapboxScrollViewBug />
+}
---
+++ MapboxScrollViewBug.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable react-native/no-inline-styles */
+import MapboxGL from "@rnmapbox/maps"
+import React, { useMemo } from "react"
+import { ScrollView, Text, View, ViewStyle, useWindowDimensions } from "react-native"
+
+import { MAPBOX_ACCESS_TOKEN } from "./.mapbox-access-token"
+
+MapboxGL.setAccessToken(MAPBOX_ACCESS_TOKEN)
+MapboxGL.setTelemetryEnabled(false)
+
+const MapboxScrollViewBug = () => {
+  const { width: windowWidth } = useWindowDimensions()
+
+  const pageContainer = useMemo(
+    (): ViewStyle => ({
+      width: windowWidth,
+      alignItems: "center",
+      justifyContent: "space-around",
+    }),
+    [windowWidth]
+  )
+
+  return (
+    <ScrollView
+      horizontal
+      pagingEnabled
+      nestedScrollEnabled
+      directionalLockEnabled
+      showsHorizontalScrollIndicator={false}
+      showsVerticalScrollIndicator={false}
+      style={{ flex: 1 }}
+    >
+      <View style={[pageContainer, { backgroundColor: "aqua" }]}>
+        <MapboxGL.MapView style={{ width: "100%", height: "50%" }} />
+        <Text>Slide 1</Text>
+      </View>
+      <View style={[pageContainer, { backgroundColor: "beige" }]}>
+        <Text>Slide 2</Text>
+      </View>
+      <View style={[pageContainer, { backgroundColor: "coral" }]}>
+        <Text>Slide 3</Text>
+      </View>
+    </ScrollView>
+  )
+}
+
+export default MapboxScrollViewBug
